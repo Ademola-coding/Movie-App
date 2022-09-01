6a4db2a0c2c1758f8039ab4b2cde08698a8dfd9e
Peter1907 <peter.nady.1408@gmail.com>
2022-09-01 17:47:48 +0200

    Hit the involvement API & the likes counter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import './style.css';
 import display from './modules/display.js';
 import showPopup from './modules/showPopup.js';
+import likes from './modules/likes.js';
 
 display();
 
 const closePopup = async () => {
   await showPopup();
-
   const closeBtn = document.querySelectorAll('.closeBtn');
-
   closeBtn.forEach((cbtn) => {
     cbtn.addEventListener('click', () => {
     });
   });
 };
 
-closePopup();
+const activateLikes = ()  => setTimeout(() => {
+  likes.displayLikes();
+  likes.avtivateLikeBtns();
+  closePopup();
+} , 1000);
+activateLikes();
--- src/modules/display.js
+++ src/modules/display.js
@@ -10,7 +10,12 @@ const getData = async () => {
 
 const display = async () => {
   const section = document.querySelector('.series');
-  await getData();
+  if (section.innerHTML) {
+    return;
+  }
+  if (list.length === 0) {
+    await getData();
+  }
   list.forEach((item) => {
     const div = document.createElement('div');
     div.classList.add('item');
@@ -18,10 +23,12 @@ const display = async () => {
       <img src="${item.image.medium}" alt="${item.name}">
       <div class="item-header">
         <h3>${item.name}</h3>
-        <span style="font-size:200%;color:red; cursor:pointer;">&hearts;</span>
+        <span class="heart" style="font-size:200%; cursor:pointer;">&hearts;</span>
       </div>
-      <div class="likes">likes</div>
-      <button data-modal-target="#modal" type="button" class="commentBtn">Comments</button><br>
+      <div class="likes">
+        <span class="num">0</span> <span>likes</span>
+      </div>
+      <button type="button" class="commentBtn">Comments</button><br>
       <button type="button">Reservations</button>
     `;
     section.appendChild(div);
